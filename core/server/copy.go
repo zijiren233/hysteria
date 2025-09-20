39dd77b144099a30e633442898391426c6a0e835
zijiren233 <pyh1670605849@gmail.com>
2025-09-20 13:18:46 +0800

    perf: copy buffer log

--- core/server/copy.go
+++ core/server/copy.go
@@ -8,23 +8,46 @@ import (
 	"github.com/apernet/hysteria/core/v2/internal/utils"
 )
 
+const (
+	trafficReportInterval  = 10 * time.Second
+	trafficReportThreshold = 1024 * 1024 // 1MB
+)
+
 var errDisconnect = errors.New("traffic logger requested disconnect")
 
 func copyBufferLog(dst io.Writer, src io.Reader, log func(n uint64) bool) error {
 	buf := make([]byte, 32*1024)
+	var unreported int
+	lastReportTime := time.Now()
+
 	for {
 		nr, er := src.Read(buf)
 		if nr > 0 {
-			if !log(uint64(nr)) {
-				// Log returns false, which means that the client should be disconnected
-				return errDisconnect
+			unreported += nr
+
+			// Check if we should report traffic
+			shouldReport := unreported >= trafficReportThreshold ||
+				time.Since(lastReportTime) >= trafficReportInterval
+
+			if shouldReport && unreported > 0 {
+				if !log(uint64(unreported)) {
+					// Log returns false, which means that the client should be disconnected
+					return errDisconnect
+				}
+				unreported = 0
+				lastReportTime = time.Now()
 			}
+
 			_, ew := dst.Write(buf[0:nr])
 			if ew != nil {
 				return ew
 			}
 		}
 		if er != nil {
+			// Report any remaining unreported traffic before returning
+			if unreported > 0 {
+				log(uint64(unreported))
+			}
 			if er == io.EOF {
 				// EOF should not be considered as an error
 				return nil
@@ -34,7 +57,12 @@ func copyBufferLog(dst io.Writer, src io.Reader, log func(n uint64) bool) error 
 	}
 }
 
-func copyTwoWayEx(id string, serverRw, remoteRw io.ReadWriter, l TrafficLogger, stats *StreamStats) error {
+func copyTwoWayEx(
+	id string,
+	serverRw, remoteRw io.ReadWriter,
+	l TrafficLogger,
+	stats *StreamStats,
+) error {
 	errChan := make(chan error, 2)
 	go func() {
 		errChan <- copyBufferLog(serverRw, remoteRw, func(n uint64) bool {
