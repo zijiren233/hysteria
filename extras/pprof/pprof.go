f7f1d4f99022e6724aa6712b6fd02a3e4c3b88a2
zijiren233 <pyh1670605849@gmail.com>
2025-09-21 16:33:15 +0800

    chore: gogc env

--- extras/pprof/pprof.go
+++ extras/pprof/pprof.go
@@ -5,8 +5,6 @@ import (
 	"net/http"
 	//nolint:gosec
 	_ "net/http/pprof"
-	"os"
-	"runtime"
 	"strconv"
 	"time"
 )
@@ -16,7 +14,6 @@ var pprofMux *http.ServeMux
 func init() {
 	pprofMux = http.DefaultServeMux
 	http.DefaultServeMux = http.NewServeMux()
-	runtime.MemProfileRate, _ = strconv.Atoi(os.Getenv("MemProfileRate"))
 }
 
 func RunPprofServer(port int) error {
